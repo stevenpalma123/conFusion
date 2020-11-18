0870e05f6ae186f20262dab71148629760647dd1
steven Palma <stevenpalma123@gmail.com>
2020-11-17 23:23:56 -0600

    NPM Scripts Part 1

---
+++ js/scripts.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+    $("#carouselButton").click(function () {
+        if ($("#carouselButton").children("span").hasClass('fa-pause')) 
+        {
+            $("#mycarousel").carousel('pause');
+            $("#carouselButton").children("span").removeClass('fa-pause');
+            $("#carouselButton").children("span").addClass('fa-play');
+        }
+        else if ($("#carouselButton").children("span").hasClass('fa-play'))
+        {
+            $("#mycarousel").carousel('cycle');
+            $("#carouselButton").children("span").removeClass('fa-play');
+            $("#carouselButton").children("span").addClass('fa-pause');
+        }
+    });
+
+    // What triggers login modal
+    $("#loginBtn").click(function () {
+        $('#loginModal').modal('show');
+    });
+
+    // What triggers Rerve Table modal
+    $("#reserveTableBtn").click(function () {
+        $('#reserveModal').modal('show');
+    });
+
+    
+});
